4ecf96f3a168d659da53eea0e3fc60a8bd038731
akash202004 <akashlaha48@gmail.com>
2025-04-15 16:26:56 +0530

    [Backend]: Intialize the basic folders and packages

---
+++ Backend/src/app.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get('/', )
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+export default app;
